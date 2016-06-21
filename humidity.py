66d3308f03e17aa547884cb83884edb6ce28e82e
Pedro Coca <pedro.coca@gmail.com>
2016-06-21 19:30:00 +0100

    Initial commit for the Sense Hat Humidity snap

---
+++ humidity.py
@@ -0,0 +1,15 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+blue = (0, 0, 255)
+white = (255, 255, 255)
+
+while True:
+    humidity = sense.humidity
+    humidity_value = 64 * humidity / 100
+    print(humidity)
+
+    pixels = [blue if i < humidity_value else white for i in range(64)]
+
+    sense.set_pixels(pixels)
